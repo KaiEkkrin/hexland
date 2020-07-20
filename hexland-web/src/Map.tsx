e34689ad95c0458dbff8c094aca5c94787d3beed
Alex Holloway <alex.holloway.ctx@gmail.com>
2020-07-20 16:00:10 +0100

    Draw a simple square grid

--- hexland-web/src/Map.tsx
+++ hexland-web/src/Map.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import './App.css';
-import './Map.css';
 import Navigation from './Navigation';
 
 import Button from 'react-bootstrap/Button';
@@ -18,35 +17,72 @@ function Controls() {
   );
 }
 
-interface IMapProps {
+interface IDrawingProps {
 }
 
 class Drawing extends React.Component {
+  mount: RefObject<HTMLDivElement>;
+
+  constructor(props: IDrawingProps) {
+    super(props);
+    this.mount = React.createRef();
+  }
+
   componentDidMount() {
-    // From https://blog.bitsrc.io/starting-with-react-16-and-three-js-in-5-minutes-3079b8829817
-    // === THREE.JS CODE START ===
+    const spacing = 75.0;
+
+    var mount = this.mount.current;
+    if (!mount) {
+      return;
+    }
+
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const left = window.innerWidth / -2;
+    const right = window.innerWidth / 2;
+    const top = window.innerHeight / -2;
+    const bottom = window.innerHeight / 2;
+    var camera = new THREE.OrthographicCamera(left, right, top, bottom, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    mount.appendChild(renderer.domElement);
+
+    // Drawing a square grid looks like this:
+    var gridMaterial = new THREE.LineBasicMaterial({ color: 0xd0d0d0 });
+
+    // Vertical lines:
+    var vPoints = [];
+    for (var x = left; x <= right; x += spacing * 2) {
+      vPoints.push(new THREE.Vector3(x, top, 1));
+      vPoints.push(new THREE.Vector3(x, bottom, 1));
+      vPoints.push(new THREE.Vector3(x + spacing, bottom, 1));
+      vPoints.push(new THREE.Vector3(x + spacing, top, 1));
+    }
+
+    var vGeometry = new THREE.BufferGeometry().setFromPoints(vPoints);
+    var vLines = new THREE.LineSegments(vGeometry, gridMaterial);
+    scene.add(vLines);
+
+    // Horizontal lines:
+    var hPoints = [];
+    for (var y = top; y <= bottom; y += spacing * 2) {
+      hPoints.push(new THREE.Vector3(left, y, 1));
+      hPoints.push(new THREE.Vector3(right, y, 1));
+      hPoints.push(new THREE.Vector3(right, y + spacing, 1));
+      hPoints.push(new THREE.Vector3(left, y + spacing, 1));
+    }
+
+    var hGeometry = new THREE.BufferGeometry().setFromPoints(hPoints);
+    var hLines = new THREE.LineSegments(hGeometry, gridMaterial);
+    scene.add(hLines);
+
     camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
+    renderer.render(scene, camera);
   }
 
   render() {
     return (
-      <div />
+      <div ref={this.mount} />
     );
   }
 }
