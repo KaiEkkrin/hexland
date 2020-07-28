d4d2f4fe121a95882b3736c9a1a7a0c8b9ea416f
Alex Holloway <alex.holloway.ctx@gmail.com>
2020-07-28 10:19:32 +0100

    remove some create-react-app default content

--- hexland-web/src/Home.tsx
+++ hexland-web/src/Home.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './Navigation';
-
-import Button from 'react-bootstrap/Button';
 
 function Home() {
   return (
     <div>
       <Navigation />
       <header className="App-header">
-        <Button>A Bootstrap button</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hexland</p>
       </header>
     </div>
   );
