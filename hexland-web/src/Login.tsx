8afc02a8d16bda9933190ab05d35e5d15a99abc6
Alex Holloway <alex.holloway.ctx@gmail.com>
2020-07-28 11:40:35 +0100

    Basic Firebase integration and Google login

--- hexland-web/src/Login.tsx
+++ hexland-web/src/Login.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { auth, googleAuthProvider } from './firebase';
 import Navigation from './Navigation';
+
+import Button from 'react-bootstrap/Button';
 
 function Login() {
   return (
     <div className="App">
       <Navigation />
       <header className="App-header">
-        <p>The login page goes here.</p>
+        <Button onClick={() => auth.signInWithPopup(googleAuthProvider)}>
+          Sign in with Google
+        </Button>
       </header>
     </div>
   );
---
+++ hexland-web/src/firebase.ts
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+
+// TODO DO NOT DO THIS
+// I should be able to get a proper, smart setup that keeps the config out
+// of github (this is only the test application here anyway...)
+// See e.g. https://firebase.googleblog.com/2017/04/easier-configuration-for-firebase-on-web.html
+// Temporarily, though, I feel it's okay to submit the hexland-test API key
+// to a private github repo
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7CCAedeMXXz2QvF-rll1b0Jlx8dhOyq4",
+  authDomain: "hexland-test.firebaseapp.com",
+  databaseURL: "https://hexland-test.firebaseio.com",
+  projectId: "hexland-test",
+  storageBucket: "hexland-test.appspot.com",
+  messagingSenderId: "1072727000053",
+  appId: "1:1072727000053:web:c044f7cd3e8521f79596bc"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export default firebase;
